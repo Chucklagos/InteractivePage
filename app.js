c374298de72abaecd545073483da1e3a76dba309
Chuck Lagos <chucklagos17@gmail.com>
2022-11-30 16:34:00 -0600

    More changes

--- app.js
+++ app.js
@@ -4,22 +4,38 @@ let scenarioDiv = document.getElementById('scenario');
 
 document.addEventListener('keydown', handleKeys)
 var scenarioLeftAdd = 0;
-function handleKeys(e){
-    let keyPress = e.code;
-    if (keyPress === 'ArrowLeft'){
-        scenarioLeftAdd += 10;
-        scenarioDiv.style.left = scenarioLeftAdd + 'px';
 
+function sleep(ms) {
+    return new Promise(
+      resolve => setTimeout(resolve, ms)
+    );
+  }
+
+async function handleKeys(e){
+    let keyPress = e.code;
+    let i = 0;
+    if (keyPress === 'ArrowLeft'){
+        while (i<100) {
+            await sleep(10);
+            scenarioLeftAdd += 15;
+            scenarioDiv.style.left = scenarioLeftAdd + 'px';  
+            i++;
+        }
         /*if (characterLeftAdd >= 1500) {
             alert ('Llego al limite');
             characterLeftAdd -=10
         }*/
 
     }
+
     if (keyPress === 'ArrowRight'){
-        scenarioLeftAdd -= 10;
+        while (i<100) {
+        await sleep(10)
+        scenarioLeftAdd -= 15;
         scenarioDiv.style.left = scenarioLeftAdd + 'px';
+        i++;
     }
+}
     /*if (scenarioLeftAdd <= 0) {
         alert ('Llego al limite');
         scenarioLeftAdd +=1
@@ -27,7 +43,12 @@ function handleKeys(e){
 
 }
 document.getElementById('rightButton').addEventListener('click', handleTouch);
-function handleTouch(e) {
-    characterLeftAdd += 10;
-    characterDiv.style.left = characterLeftAdd + 'px';
+async function handleTouch(e) {
+    let i = 0;
+    while (i<100) {
+        await sleep(10);
+        scenarioLeftAdd -= 15;
+        scenarioDiv.style.left = scenarioLeftAdd + 'px';  
+        i++;
+    }
 }
